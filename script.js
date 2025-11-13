545ffff47e0f593a8284f04830fd83addc5f1aaf
Didier_AGT <didieragt@gmail.com>
2025-11-13 13:51:53 -0500

    Fra628,629

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ menOpc.style.display = 'none';
 let codFra = document.getElementById('codFra');
 
 function SeleccionFra (numFra){
-    let arr_Fras = ['624','625','626','627'];
+    let arr_Fras = ['624','625','626','627','628','629'];
     //deja una opcion por default en el select
     let defaultOpc = document.createElement('option');
     defaultOpc.textContent = '----';
